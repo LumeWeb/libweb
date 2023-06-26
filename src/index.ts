a813ee35f359a6f019d53b3bad0d9585dcc0d23e
Derrick Hammer <derrick@derrickhammer.com>
2023-06-26 00:39:25 -0400

    feat: re-export most of noble utility functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
 import { ed25519 } from "@noble/curves/ed25519";
 import { sha512 } from "@noble/hashes/sha512";
 
+export {
+  bytesToHex,
+  hexToBytes,
+  toBytes,
+  concatBytes,
+  randomBytes,
+} from "@noble/hashes/utils";
+export {
+  numberToHexUnpadded,
+  hexToNumber,
+  bytesToNumberBE,
+  bytesToNumberLE,
+  numberToBytesBE,
+  numberToBytesLE,
+  numberToVarBytesBE,
+  ensureBytes,
+  equalBytes,
+  utf8ToBytes,
+  bitLen,
+  bitGet,
+  bitSet,
+  bitMask,
+} from "@noble/curves/abstract/utils";
 export * from "./err.js";
 export * from "./errTracker.js";
 export * from "./objAsString.js";
