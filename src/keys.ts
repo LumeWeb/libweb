7fefaf0818417f1cc4ae0c9f360e5b08734d68f3
Derrick Hammer <derrick@derrickhammer.com>
2023-08-20 02:35:17 -0400

    feat: change deriveChildKey to hkdf sha256 and create deriveBlakeChildKey that hashes based on the initial blake3 route used by s5

--- src/keys.ts
+++ src/keys.ts
@@ -1,7 +1,16 @@
 import { blake3 } from "@noble/hashes/blake3";
 import { concatBytes } from "@noble/hashes/utils";
+import { hkdf } from "@noble/hashes/hkdf";
+import { sha256 } from "@noble/hashes/sha256";
 
 export function deriveChildKey(
+  parentKey: Uint8Array,
+  tweak: string,
+): Uint8Array {
+  return hkdf(sha256, parentKey, undefined, tweak, 32);
+}
+
+export function deriveBlakeChildKey(
   parentKey: Uint8Array,
   tweak: string,
 ): Uint8Array {
